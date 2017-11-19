104ddb9b31d9bac1949c56f4db9ade15c1dedf82
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-11-19 18:31:12 -0500

    Initial commit

---
+++ lib/wallaby_test/repo.ex
@@ -0,0 +1,11 @@
+defmodule WallabyTest.Repo do
+  use Ecto.Repo, otp_app: :wallaby_test
+
+  @doc """
+  Dynamically loads the repository url from the
+  DATABASE_URL environment variable.
+  """
+  def init(_, opts) do
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  end
+end
